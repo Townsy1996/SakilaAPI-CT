65dd13ef8b6b1657c9275da5e17792aa772949a2
corey <corey_townsend@outlook.com>
2024-03-05 11:31:52 +0000

    update filmrepo

--- src/main/java/com/example/sakiladb/repositories/FilmRepository.java
+++ src/main/java/com/example/sakiladb/repositories/FilmRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface FilmRepository extends JpaRepository<Film, Short> {
-    @Query("SELECT film FROM Film film JOIN film.actors actor WHERE actor.id = :id")
-    Iterable<Film> findByActorId(@Param("id") Short id);
+    @Query("SELECT film FROM Film film JOIN film.actors actor WHERE actor.actorId = :id")
+    Iterable<Film> findByActorId(@Param("id") int id);
 
     @Query("SELECT film FROM Film film JOIN film.categorySet cat WHERE cat.name = :catName")
     Iterable<Film> findByCategoryName(@Param("catName") String catName);
