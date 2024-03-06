b1188ddf5da26bd96ae7621df64b1841306cfa24
corey <corey_townsend@outlook.com>
2024-03-06 13:50:08 +0000

    cucumber tests config

--- src/test/java/com/example/sakiladb/Steps/AddActorSteps.java
+++ src/test/java/com/example/sakiladb/Steps/AddActorSteps.java
@@ -3,15 +3,12 @@ import com.example.sakiladb.entities.Actor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
-import org.springframework.test.context.ContextConfiguration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AddActorSteps {
 
     @Autowired
